cff065da91e45039e986f93c3de4d21a92c21235
Knyazev Nikita <nk8222138@gmail.com>
2024-12-16 02:30:42 +0300

    sc

--- Decorator_1/Decorator_1.cpp
+++ Decorator_1/Decorator_1.cpp
@@ -10,17 +10,19 @@ public:
    virtual void render(const std::string& data_1, const std::string& data_2) const {
       std::cout << data_2;
    }
+
+   virtual ~Text() = default;
 };
 
 class DecoratedText : public Text {
 public:
-   explicit DecoratedText(Text* text) : text_(text) {}
-   Text* text_;
+   explicit DecoratedText(std::shared_ptr<Text> text) : text_(text) {}
+   std::shared_ptr<Text> text_;
 };
 
 class ItalicText : public DecoratedText {
 public:
-   explicit ItalicText(Text* text) : DecoratedText(text) {}
+   explicit ItalicText(std::shared_ptr<Text> text) : DecoratedText(text) {}
    void render(const std::string& data)  const {
       std::cout << "<i>";
       text_->render(data);
@@ -30,7 +32,7 @@ public:
 
 class BoldText : public DecoratedText {
 public:
-   explicit BoldText(Text* text) : DecoratedText(text) {}
+   explicit BoldText(std::shared_ptr<Text> text) : DecoratedText(text) {}
    void render(const std::string& data) const {
       std::cout << "<b>";
       text_->render(data);
@@ -40,7 +42,7 @@ public:
 
 class Paragraph : public DecoratedText {
 public:
-   explicit Paragraph(Text* text) : DecoratedText(text) {}
+   explicit Paragraph(std::shared_ptr<Text> text) : DecoratedText(text) {}
    void render(const std::string& data) const {
       std::cout << "<p>";
       text_->render(data);
@@ -50,7 +52,7 @@ public:
 
 class Reversed : public DecoratedText {
 public:
-   explicit Reversed(Text* text) : DecoratedText(text) {}
+   explicit Reversed(std::shared_ptr<Text> text) : DecoratedText(text) {}
    void render(const std::string& data) const {
       std::string copy{ data };
       std::reverse(copy.begin(), copy.end());
@@ -60,7 +62,7 @@ public:
 
 class Link : public DecoratedText {
 public:
-   explicit Link(Text* text) : DecoratedText(text) {}
+   explicit Link(std::shared_ptr<Text> text) : DecoratedText(text) {}
    void render(const std::string& data_1, const std::string& data_2) const {
       std::cout << "<a href=" << data_1 << ">";
       text_->render(data_2);
@@ -69,15 +71,17 @@ public:
 };
 
 int main() {
-   auto text_block = new ItalicText(new BoldText(new Text()));
+
+   auto text_block = std::make_shared<ItalicText>(std::make_shared<BoldText> (std::make_shared<Text>()));
    text_block->render("Hello world");
 
-   auto text_block_1 = new Paragraph(new Text());
+   auto text_block_1 = std::make_shared<Paragraph>(std::make_shared<Text>());
    text_block_1->render("Hello world");
 
-   auto text_block_2 = new Reversed(new Text());
+   auto text_block_2 = std::make_shared<Reversed>(std::make_shared<Text>());
    text_block_2->render("Hello world");
 
-   auto text_block_3 = new Link(new Text());
+   auto text_block_3 = std::make_shared<Link>(std::make_shared<Text>());
    text_block_3->render("netology.ru", "Hello world");
+
 }
